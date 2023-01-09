b1f7fdf41a63d82ce04bbecf52282e1fc70c3628
Violeta Mincheva <118520945+VioletaMincheva@users.noreply.github.com>
2023-01-09 14:50:06 +0200

    Add files via upload

---
+++ Inheritance/Person_ex1/Child.java
@@ -0,0 +1,10 @@
+package Inheritance.Person_ex1;
+
+public class Child extends Person{
+
+    public Child(String name, int age) {
+        super(name, age);
+    }
+
+
+}
---
+++ Inheritance/Person_ex1/Person.java
@@ -0,0 +1,19 @@
+package Inheritance.Person_ex1;
+
+public class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
