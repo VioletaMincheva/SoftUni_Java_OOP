c86c8e0cdbad569ef86e685b48f5a65c3b8f5c77
Violeta Mincheva <118520945+VioletaMincheva@users.noreply.github.com>
2023-01-09 13:49:48 +0200

    Add files via upload

---
+++ Inheritance/HierarchicalInheritance_lab3/Cat.java
@@ -0,0 +1,8 @@
+package Inheritance.HierarchicalInheritance_lab3;
+
+public class Cat extends Animal {
+
+    public void meow() {
+        System.out.println("meowing...");
+    }
+}
---
+++ Inheritance/HierarchicalInheritance_lab3/Dog.java
@@ -0,0 +1,7 @@
+package Inheritance.HierarchicalInheritance_lab3;
+
+public class Dog extends Animal {
+    public void bark() {
+        System.out.println("barking...");
+    }
+}
