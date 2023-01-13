0597af43546e56f3765c5b1cc3a0834ed50d6535
Violeta Mincheva <118520945+VioletaMincheva@users.noreply.github.com>
2023-01-13 16:50:37 +0200

    Add files via upload

---
+++ Polymorphism/Animals_lab3/Dog.java
@@ -0,0 +1,13 @@
+package Polymorphism.Animals_lab3;
+
+public class Dog extends Animal {
+
+    public Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return super.explainSelf() + System.lineSeparator() + "DJAAF";
+    }
+}
