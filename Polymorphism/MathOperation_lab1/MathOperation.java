42f110010c9b7f91365b2bb953d4bb66cd5bba37
Violeta Mincheva <118520945+VioletaMincheva@users.noreply.github.com>
2023-01-13 16:35:07 +0200

    Add files via upload

---
+++ Polymorphism/MathOperation_lab1/MathOperation.java
@@ -0,0 +1,15 @@
+package Polymorphism.MathOperation_lab1;
+
+public class MathOperation {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+}
