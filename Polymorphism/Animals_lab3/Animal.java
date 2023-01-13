da4d4c00391f2c64bdb82379ddce002b2b289ac0
Violeta Mincheva <118520945+VioletaMincheva@users.noreply.github.com>
2023-01-13 16:50:10 +0200

    Add files via upload

---
+++ Polymorphism/Animals_lab3/Animal.java
@@ -0,0 +1,31 @@
+package Polymorphism.Animals_lab3;
+
+public abstract class Animal {
+    private String name;
+    private String favouriteFood;
+
+    protected Animal(String name, String favouriteFood) {
+       this.setName(name);
+       this.setFavouriteFood(favouriteFood);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    protected void setFavouriteFood(String favouriteFood) {
+        this.favouriteFood = favouriteFood;
+    }
+
+    public String explainSelf(){
+        return String.format("I am %s and my favourite food is %s", this.getName(), this.getFavouriteFood());
+    };
+}
