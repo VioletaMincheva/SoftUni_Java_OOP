80d658d6b094fdb6ae44e88ca813269c3077044d
Violeta Mincheva <118520945+VioletaMincheva@users.noreply.github.com>
2023-01-18 14:40:16 +0200

    Add files via upload

---
+++ Polymorphism/WildFarm_ex3/Meat.java
@@ -0,0 +1,8 @@
+package Polymorphism.WildFarm_ex3;
+
+public class Meat extends Food{
+
+    public Meat(Integer quantity) {
+        super(quantity);
+    }
+}
---
+++ Polymorphism/WildFarm_ex3/Vegetable.java
@@ -0,0 +1,9 @@
+package Polymorphism.WildFarm_ex3;
+
+public class Vegetable extends Food{
+
+    public Vegetable(Integer quantity) {
+        super(quantity);
+    }
+
+}
