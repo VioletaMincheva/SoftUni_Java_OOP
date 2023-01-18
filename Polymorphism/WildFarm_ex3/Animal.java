7ac62a61028658ddb22478a2e34024ee06e4c51e
Violeta Mincheva <118520945+VioletaMincheva@users.noreply.github.com>
2023-01-18 14:37:12 +0200

    Add files via upload

---
+++ Polymorphism/WildFarm_ex3/Animal.java
@@ -0,0 +1,54 @@
+package Polymorphism.WildFarm_ex3;
+
+public abstract class Animal {
+    private String animalName;
+    private String animalType;
+    private Double animalWeight;
+    private Integer foodEaten;
+
+    public Animal(String animalName, String animalType, Double animalWeight) {
+        setAnimalName(animalName);
+        setAnimalType(animalType);
+        setAnimalWeight(animalWeight);
+        foodEaten = 0;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+
+    public Double getAnimalWeight() {
+        return animalWeight;
+    }
+
+    public void setAnimalWeight(Double animalWeight) {
+        this.animalWeight = animalWeight;
+    }
+
+    public Integer getFoodEaten() {
+        return foodEaten;
+    }
+
+    public void setFoodEaten(Integer foodEaten) {
+        this.foodEaten = foodEaten;
+    }
+
+    public abstract void makeSound();
+
+    protected void eat(Food food) {
+        this.setFoodEaten(this.foodEaten + food.getQuantity());
+    }
+
+}
