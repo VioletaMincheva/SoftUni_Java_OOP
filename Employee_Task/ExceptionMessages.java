62e9312fd725b776644109fc407d0e61ace47b0b
Violeta Mincheva <118520945+VioletaMincheva@users.noreply.github.com>
2023-03-10 17:07:27 +0200

    Add files via upload

---
+++ Employee_Task/ExceptionMessages.java
@@ -0,0 +1,9 @@
+package EmployeeApp;
+
+public class ExceptionMessages {
+    public static final String INVALID_NAME = "Name cannot be null or white space!";
+    public static final String INVALID_SALARY = "Salary cannot be zero or negative!";
+    public static final String INVALID_TITLE = "Title cannot be null or white space!";
+    public static final String INVALID_DESCRIPTION = "Description cannot be null or white space!";
+    public static final String INVALID_STATUS = "Status can be yes or no!";
+}
